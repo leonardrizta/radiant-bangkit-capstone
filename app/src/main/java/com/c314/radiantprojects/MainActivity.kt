f3b097f45ec29d4b4028286f30ebe6a01d878847
raden-dimas-012 <a1181504@bangkit.academy>
2021-05-17 08:03:24 +0700

    adding kotlin project

---
+++ app/src/main/java/com/c314/radiantprojects/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.c314.radiantprojects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
