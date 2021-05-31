5919b19b6b551194fdac7bf4ee5ecf40e43af3cb
Leonard Rizta Anugerah Perdana <leonardrizta3@gmail.com>
2021-06-01 00:10:01 +0700

    preprocess image to RGB before predict

--- backend/cloud/app.py
+++ backend/cloud/app.py
@@ -5,6 +5,8 @@ from skimage import transform, io
 import numpy as np
 import os
 from keras.models import load_model
+from PIL import Image
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -12,16 +14,21 @@ UPLOAD_FOLDER = 'static/uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'tiff', 'webp'}
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 def auc(y_true, y_pred):
     auc = tf.metrics.auc(y_true, y_pred)[1]
     keras.backend.get_session().run(tf.local_variables_initializer())
     return auc
 
+
 @app.route('/')
 def main():
     return 'This is Radiant Team API for predict skin disease'
+
 
 @app.route('/api/predict', methods=['POST'])
 def recognize_image():
@@ -39,25 +46,35 @@ def recognize_image():
             errors["message"] = 'File type of {} is not allowed'.format(file.filename)
     img_url = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
+    # convert image to RGB
+    img = Image.open(img_url).convert('RGB')
+    now = datetime.now()
+    image_predict = 'static/uploads/' + now.strftime("%H%M%S") + ".png"
+    img.convert('RGB').save(image_predict, format="png")
+    img.close()
+
     # load model for prediction
     model = load_model('dermnet.h5', custom_objects={'auc': auc})
-    
+
     # prepare image for prediction
-    img_array = io.imread(img_url, as_gray=False)
-    small_grey = transform.resize(img_array, (300,300), mode='symmetric', preserve_range=True) 
+    img_array = io.imread(image_predict, as_gray=False)
+    small_grey = transform.resize(img_array, (300, 300), mode='symmetric', preserve_range=True)
     img_to_predict = np.expand_dims(small_grey / 255.0, 0)
-    
+
     # predict
     prediction_array = model.predict(img_to_predict)
-        
+
     # prepare api response
-    class_names = ['Acne and Rosacea Photos', 'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions', 'Eczema Photos', 'Light Diseases and Disorders of Pigmentation', 'Nail Fungus and other Nail Disease', 'Vascular Tumors', 'Warts Molluscum and other Viral Infections']
+    class_names = ['Acne and Rosacea Photos', 'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions',
+                   'Eczema Photos', 'Light Diseases and Disorders of Pigmentation',
+                   'Nail Fungus and other Nail Disease', 'Vascular Tumors',
+                   'Warts Molluscum and other Viral Infections']
     result = {
-        "prediction" : class_names[np.argmax(prediction_array)],
-        "confidence" : '{:2.0f}%'.format(100*np.max(prediction_array))
+        "prediction": class_names[np.argmax(prediction_array)],
+        "confidence": '{:2.0f}%'.format(100 * np.max(prediction_array))
     }
-    
-    return jsonify(isError= False, message= "Success", statusCode= 200, data=result), 200
+
+    return jsonify(isError=False, message="Success", statusCode=200, data=result), 200
 
 
 if __name__ == '__main__':
